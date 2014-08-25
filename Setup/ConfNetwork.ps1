c2734f20d426df699bb81dd7c681f877bc177fcd
undercoverdave <undercoverdave@gmail.com>
2014-08-25 14:10:03 -0500

    Added network setting script

---
+++ Setup/ConfNetwork.ps1
@@ -0,0 +1,12 @@
+# Set static IP address 
+$ipaddress = "172.17.122.11" 
+$ipprefix = "24" 
+$ipgw = "172.17.122.1" 
+$ipdns = "172.17.122.11" 
+$ipif = (Get-NetAdapter).ifIndex 
+New-NetIPAddress -IPAddress $ipaddress -PrefixLength $ipprefix -InterfaceIndex $ipif -DefaultGateway $ipgw 
+# Rename the computer 
+$newname = "dc1" 
+Rename-Computer -NewName $newname -force 
+# Restart
+Restart
